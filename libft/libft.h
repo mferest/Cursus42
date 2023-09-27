7d7faeb20a83eceecfaf3c814dc42fb181960e5a
mferest <139508718+mferest@users.noreply.github.com>
2023-09-27 20:06:16 +0200

    Echo asta split

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: manufern <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 16:27:43 by manufern          #+#    #+#             */
-/*   Updated: 2023/09/23 12:43:29 by manufern         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:46:16 by manufern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int		ft_toupper(int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
