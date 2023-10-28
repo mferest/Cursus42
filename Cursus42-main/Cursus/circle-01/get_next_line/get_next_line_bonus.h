5f789948b4c7bdaf89ccb2f2600f68e56f322402
mferest <139508718+mferest@users.noreply.github.com>
2023-10-28 18:24:14 +0200

    Add files via upload

--- Cursus42-main/Cursus/circle-01/get_next_line/get_next_line_bonus.h
+++ Cursus42-main/Cursus/circle-01/get_next_line/get_next_line_bonus.h
@@ -19,6 +19,7 @@
 # include<fcntl.h>
 # include<limits.h>
 
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
@@ -29,4 +30,6 @@ char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *s1);
 char	*ft_strchr(char *s, int c);
 char	*ft_substr(char *s);
+
+
 #endif
