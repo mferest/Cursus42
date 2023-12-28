697b58561090aea2f4c8206dc49b598b2b3c61bf
mferest <manueljudichini@gmail.com>
2023-12-28 13:34:29 +0100

    areglos menores de drow_line

--- Cursus42-main/Cursus/circle-02/fdf/4_drow_map.c
+++ Cursus42-main/Cursus/circle-02/fdf/4_drow_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   4_drow_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manufern <manufern@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:30:38 by manufern          #+#    #+#             */
-/*   Updated: 2023/12/27 18:25:06 by manufern         ###   ########.fr       */
+/*   Updated: 2023/12/28 13:34:10 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,44 +24,35 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int ft_scale_x(t_map **map)
+void ft_drow_line(t_map *map, int center_x, int center_y, t_data *img)
 {
-    t_map *map_aux = *map;
-    int x_max = 0;
-    int x_min = 2147483647;
-    while(map_aux)
-    {
-        if(x_max < map_aux->x_rotate )
-            x_max = map_aux->x_rotate;
-        if (x_min < map_aux->x_rotate)
-             x_min = map_aux->x_rotate;
-        map_aux = map_aux->next;
-    }
-    return (WIDTH / (x_max - x_min));
-}
+    t_map   *line1;
+    t_map   *line2;
 
-int ft_scale_y(t_map **map)
-{
-    t_map *map_aux = *map;
-    int y_max = 0;
-    int y_min = 2147483647;
-    while(map_aux)
+    line1 = map;
+    line2 = map;
+    while (line2 != NULL && line2->y != 1)
     {
-        if(y_max < map_aux->y_rotate )
-            y_max = map_aux->y_rotate;
-        if (y_min < map_aux->y_rotate)
-             y_min = map_aux->y_rotate;
-        map_aux = map_aux->next;
+        line2 = line2->next;
     }
-    return (HEIGHT / (y_max - y_min));
+    while (line2 || ( line1 && line1->next))
+    {
+        if (line1->next != NULL && line1->y == line1->next->y)
+            line_bresenham(img, line1->x_rotate + center_x, line1->y_rotate + center_y, line1->next->x_rotate + center_x, line1->next->y_rotate + center_y, line1->color, line1->next->color);
+        if (line2)
+            line_bresenham(img, line1->x_rotate + center_x, line1->y_rotate + center_y, line2->x_rotate + center_x, line2->y_rotate + center_y, line1->color, line2->color);
+        line1 = line1->next;
+        if (line2)
+            line2 = line2->next;
+    }
 }
 
 void ft_drow(t_map *map, float center_x, float center_y)
 {
-    void *mlx;
-    void *mlx_win;
-    t_data img;
-    t_map *map_aux;
+    void    *mlx;
+    void    *mlx_win;
+    t_data  img;
+    t_map   *map_aux;
 
     mlx = mlx_init();
     mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "FDF");
@@ -75,75 +66,7 @@ void ft_drow(t_map *map, float center_x, float center_y)
             my_mlx_pixel_put(&img, map_aux->x_rotate + center_x, map_aux->y_rotate + center_y, map_aux->color);
         map_aux = map_aux->next;
     }
-    map_aux = map;
-    while (map_aux && map_aux->next)
-    {
-        if (map_aux->next != NULL && map_aux->y == map_aux->next->y)
-        {
-            line_bresenham(&img, map_aux->x_rotate + center_x, map_aux->y_rotate + center_y, map_aux->next->x_rotate + center_x, map_aux->next->y_rotate + center_y, map_aux->color, map_aux->next->color);
-        }
-        map_aux = map_aux->next;
-    }
-    t_map *line1 = map;
-    t_map *line2 = map;
-
-    while (line2 != NULL && line2->y != 1)
-    {
-        line2 = line2->next;
-    }
-    while (line2)
-    {
-        line_bresenham(&img, line1->x_rotate + center_x, line1->y_rotate + center_y, line2->x_rotate + center_x, line2->y_rotate + center_y, line1->color, line2->color);
-        line1 = line1->next;
-        line2 = line2->next;
-    }
+    ft_drow_line(map, center_x, center_y, &img);
     mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
 }
-
-/* void ft_drow(t_map *map, float center_x, float center_y)
-{
-    void *mlx;
-    void *mlx_win;
-    t_data img;
-    t_map *map_aux;
-
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "FDF");
-    img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    map_aux = map;
-    int scale_x = ft_scale_x(&map);
-    int scale_y = ft_scale_y(&map); 
-    while (map_aux)
-    {
-        if (map_aux->x_rotate + center_x <= WIDTH && map_aux->y_rotate + center_y <= HEIGHT 
-            && map_aux->x_rotate + center_x >= 0 && map_aux->y_rotate + center_y >= 0)
-            my_mlx_pixel_put(&img, map_aux->x_rotate * scale_x, map_aux->y_rotate * scale_y, map_aux->color);
-        map_aux = map_aux->next;
-    }
-    map_aux = map;
-    while (map_aux && map_aux->next)
-    {
-        if (map_aux->next != NULL && map_aux->y == map_aux->next->y)
-        {
-            line_bresenham(&img, map_aux->x_rotate * scale_x, map_aux->y_rotate * scale_y, map_aux->next->x_rotate * scale_x, map_aux->next->y_rotate * scale_y, map_aux->color, map_aux->next->color);
-        }
-        map_aux = map_aux->next;
-    }
-    t_map *line1 = map;
-    t_map *line2 = map;
-
-    while (line2 != NULL && line2->y != 1)
-    {
-        line2 = line2->next;
-    }
-    while (line2)
-    {
-        line_bresenham(&img, line1->x_rotate * scale_x, line1->y_rotate * scale_y, line2->x_rotate * scale_x, line2->y_rotate * scale_y, line1->color, line2->color);
-        line1 = line1->next;
-        line2 = line2->next;
-    }
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-    mlx_loop(mlx);
-}  */
